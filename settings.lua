4ef3e62ddcd95f05f493d7473502bfe8dd5df564
berengma <Gundul@mailbox.org>
2020-12-11 13:05:21 +0100

    fix default gain value

--- settings.lua
+++ settings.lua
@@ -6,7 +6,7 @@
 farmingNG.havetech = minetest.get_modpath("technic")		-- Is technic mod present ? If not use wearout tool instead
 --
 --
-farmingNG.seeder_max_charge      = tonumber(minetest.settings:get("farmingNG_seeder_max_charge")) or 900000      				-- Maximum charge of the seeder 
+farmingNG.seeder_max_charge      = tonumber(minetest.settings:get("farmingNG_seeder_max_charge")) or 900000					-- Maximum charge of the seeder 
 farmingNG.seeder_charge_per_node = tonumber(minetest.settings:get("farmingNG_seeder_charge_per_node")) or 1800				-- How much it costs to place a seed
 --
 farmingNG.chaton = minetest.settings:get_bool("farmingNG_chaton", false)												-- more verbose chat messages
@@ -16,4 +16,4 @@ farmingNG.harvester_max_charge     	= tonumber(minetest.settings:get("farmingNG_
 farmingNG.harvester_charge_per_node	= tonumber(minetest.settings:get("farmingNG_harvester_charge_per_node")) or 1300 		-- Costs of harvesting one node
 farmingNG.harvester_machine 			= minetest.settings:get_bool("farmingNG_harvester_machine", true)    					-- make the tool available
 farmingNG.harvester_nofullg    		= minetest.settings:get_bool("farmingNG_harvester_nofullg", true)    					-- harvests also wheat_7, rice_7 etc no need to wait until full grown 
-farmingNG.gain						= tonumber(minetest.settings:get("farmingNG_gain")) or 1.0							-- control sound volume
+farmingNG.gain						= tonumber(minetest.settings:get("farmingNG_gain")) or 0.5							-- control sound volume
