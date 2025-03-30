76734ebfd3cc939ec74d07574327069c9916169e
Gundul <sampah@mailbox.org>
2025-03-30 15:15:12 +0200

    switch to new namespace

--- machines/plough.lua
+++ machines/plough.lua
@@ -6,7 +6,7 @@ end
 
 local perNode = 65535 / (farmingNG.plough_max_charge / farmingNG.plough_charge_per_node)
 
-minetest.register_entity("farming_nextgen:pos", 
+core.register_entity("farming_nextgen:pos", 
 {
 	initial_properties = {
 		visual = "cube",
@@ -31,7 +31,7 @@ minetest.register_entity("farming_nextgen:pos",
 
 local function getDecorations()
 	local deco = {}
-		for name, def in pairs(minetest.registered_nodes) do
+		for name, def in pairs(core.registered_nodes) do
 			if def.groups["plant"] or def.groups["seed"] or def.groups["grass"] or def.groups["flower"] then
 				table.insert(deco, name)
 			end
@@ -66,7 +66,7 @@ local function plough(min, max, charge)
 			local water = vector.new(x, min.y - 1, z)
 			local sec = vector.new(x, min.y - 2, z)
 
-			--minetest.add_entity(tmp, "farming_nextgen:pos")
+			--core.add_entity(tmp, "farming_nextgen:pos")
 			node = core.get_node(tmp)
 			if node.name == "ignore" then
 				return
@@ -79,10 +79,10 @@ local function plough(min, max, charge)
 			if farmingNG.plough_set_water_nodes then
 				if ((math.fmod(max.z - z, 4) == 0) or
 					(math.fmod(max.x - x, 4) == 0)) and
-					not minetest.find_node_near(tmp, 3,
+					not core.find_node_near(tmp, 3,
 					{name = 'default:water_source'}) then
-						minetest.set_node(water, {name = "default:water_source"})
-						minetest.set_node(sec, {name = "default:stone"})
+						core.set_node(water, {name = "default:water_source"})
+						core.set_node(sec, {name = "default:stone"})
 				end
 			end
 			z = z + 1
@@ -117,27 +117,27 @@ local function isAreaClean(min, max)
 	end
 	min, max = vector.sort(min, max)
 	volume = (1 + max.x - min.x) * (1 + max.z - min.z)
-	nodes = #minetest.find_nodes_in_area_under_air(min, max, "group:soil")
-	--minetest.chat_send_all("CNT: "..volume.."  REL: "..nodes)
+	nodes = #core.find_nodes_in_area_under_air(min, max, "group:soil")
+	--core.chat_send_all("CNT: "..volume.."  REL: "..nodes)
 	return (volume == nodes)
 end
 
 local function debug(current, pos1, pos2)
-	minetest.chat_send_all(orange("Current: "..dump(current)))
+	core.chat_send_all(orange("Current: "..dump(current)))
 	if pos1 then
-		minetest.chat_send_all("Pos1: " ..minetest.pos_to_string(pos1))
+		core.chat_send_all("Pos1: " ..core.pos_to_string(pos1))
 	else
-		minetest.chat_send_all("Pos1: NIL")
+		core.chat_send_all("Pos1: NIL")
 	end
 	if pos2 then
-		minetest.chat_send_all("Pos2: " ..minetest.pos_to_string(pos2))
+		core.chat_send_all("Pos2: " ..core.pos_to_string(pos2))
 	else
-		minetest.chat_send_all("Pos2: NIL")
+		core.chat_send_all("Pos2: NIL")
 	end
 end
 
 local function delete_pos(pos)
-	local objects = minetest.get_objects_inside_radius(pos, 1)
+	local objects = core.get_objects_inside_radius(pos, 1)
 
 	for i = 1, #objects, 1 do
 		local obj = objects[i]
@@ -153,11 +153,11 @@ local function showMarkers(pos1, pos2)
 	end
 	if pos1 then
 		delete_pos(pos1)
-		minetest.add_entity(pos1, "farming_nextgen:pos")
+		core.add_entity(pos1, "farming_nextgen:pos")
 	end
 	if pos2 then
 		delete_pos(pos2)
-		minetest.add_entity(pos2, "farming_nextgen:pos")
+		core.add_entity(pos2, "farming_nextgen:pos")
 	end
 end
 
@@ -186,10 +186,10 @@ local function isFreeFromProtections(pos1, pos2, name)
 	if checkAreaUp then
 		checkAreaUp = vector.add(pos1, vector.new(0, 2, 0))
 	end
-	if minetest.is_area_protected(checkAreaUp, checkAreaDown, name or "", 1) then
+	if core.is_area_protected(checkAreaUp, checkAreaDown, name or "", 1) then
 		if name then
-			minetest.chat_send_player(name, orange("There are already other players protections in this area"))
-	    	minetest.record_protection_violation(checkAreaDown, name)
+			core.chat_send_player(name, orange("There are already other players protections in this area"))
+	    	core.record_protection_violation(checkAreaDown, name)
 		end
 		return false
 	end
@@ -199,20 +199,20 @@ end
 local function is_valid(pos1, pos2, name)
 	if not pos1 or not pos2 then
 		if name then
-			minetest.chat_send_player(name, orange("One or more positions not set."))
+			core.chat_send_player(name, orange("One or more positions not set."))
 		end
 		return false
 	end
 	if (pos1.y ~= pos2.y ) then
 		if name then
-			minetest.chat_send_player(name, orange("You have to use your plough on level."))
+			core.chat_send_player(name, orange("You have to use your plough on level."))
 		end
 		return false
 	end
 	if (math.abs(pos1.x - pos2.x) * math.abs(pos1.z - pos2.z) > 
 		(farmingNG.plough_max_charge / farmingNG.plough_charge_per_node)) then
 			if name then
-				minetest.chat_send_player(name, orange("Choosen area is too big."))
+				core.chat_send_player(name, orange("Choosen area is too big."))
 			end
 			return false
 	end
@@ -228,8 +228,8 @@ local onPlace =  function(itemstack, placer, pointed_thing)
 	local meta = itemstack:get_meta()
 	local node = nil
 	local current = meta:get_int("current")
-	local pos1 = minetest.deserialize(meta:get("pos1") or "")
-	local pos2 = minetest.deserialize(meta:get("pos2") or "")
+	local pos1 = core.deserialize(meta:get("pos1") or "")
+	local pos2 = core.deserialize(meta:get("pos2") or "")
 
 	--debug(current, pos1, pos2)
 	if pointed_thing.type ~= "node" then
@@ -238,9 +238,9 @@ local onPlace =  function(itemstack, placer, pointed_thing)
 	-- on_place from this tool and on_rightclick from node do not go together
 	-- well. To enable the anvil repair, we get it's registration and call
 	-- it's method on_rightclick right away!
-	node = minetest.get_node(pointed_thing.under)
+	node = core.get_node(pointed_thing.under)
 	if not farmingNG.havetech and node.name == "anvil:anvil" then
-		local nodedef = minetest.registered_nodes[node.name]
+		local nodedef = core.registered_nodes[node.name]
 		nodedef.on_rightclick(pointed_thing.under, node, placer, itemstack, pointed_thing)
 		return itemstack
 	end
@@ -256,11 +256,11 @@ local onPlace =  function(itemstack, placer, pointed_thing)
 			delete_pos(pos1)	
 			return itemstack
 		end
-		meta:set_string("pos1", minetest.serialize(pos1))
+		meta:set_string("pos1", core.serialize(pos1))
 		if (not is_valid(pos1, pos2, name)) then
 			meta:set_string("pos2", "")
 		end
-		minetest.add_entity(pos1, "farming_nextgen:pos")
+		core.add_entity(pos1, "farming_nextgen:pos")
 		meta:set_int("current", 2)
 		return itemstack
 	end
@@ -284,7 +284,7 @@ local onPlace =  function(itemstack, placer, pointed_thing)
 			delete_pos(pos1)	
 			return itemstack
 		end
-		meta:set_string("pos2", minetest.serialize(pos2))
+		meta:set_string("pos2", core.serialize(pos2))
 		if (not is_valid(pos1, pos2, name)) then
 			meta:set_string("pos2", "")
 			meta:set_int("current", 2)
@@ -307,11 +307,11 @@ end
 local onUse = function(itemstack, user, pointed_thing)
 	local name = user:get_player_name()
 	local meta = itemstack:get_meta()
-   	local privs = minetest.get_player_privs(name)
+   	local privs = core.get_player_privs(name)
 	local charge = 0
 	local pos = pointed_thing.under
-	local areaMin = minetest.deserialize(meta:get("pos1"))
-	local areaMax = minetest.deserialize(meta:get("pos2"))
+	local areaMin = core.deserialize(meta:get("pos1"))
+	local areaMax = core.deserialize(meta:get("pos2"))
      
 	if farmingNG.havetech then
 		charge =  meta:get_int("technic:charge")
@@ -322,34 +322,34 @@ local onUse = function(itemstack, user, pointed_thing)
     if pointed_thing.type ~= "node" then
 	    return itemstack
     end
-	--minetest.chat_send_all("Charge = "..dump(charge))
+	--core.chat_send_all("Charge = "..dump(charge))
     if not charge or  
 		    charge < farmingNG.plough_charge_per_node then
-		    minetest.chat_send_player(name, orange(S(" *** Your device needs to be serviced")))
+		    core.chat_send_player(name, orange(S(" *** Your device needs to be serviced")))
 	    return
     end
 	if not meta or not (meta:contains("pos1") and meta:contains("pos2")) then
-		minetest.chat_send_player(name, orange("You need to set positions first"))
+		core.chat_send_player(name, orange("You need to set positions first"))
 		return itemstack
 	end
 	if isNearArea(pos, areaMin, areaMax) then
 		showMarkers(areaMin, areaMax)
 	end
 	if not isInArea(pos, areaMin, areaMax) then
-		minetest.chat_send_player(name, orange("You have to click in your defined area"))
+		core.chat_send_player(name, orange("You have to click in your defined area"))
 		return
 	end
     if not isFreeFromProtections(areaMin, areaMax, name) then
 	    return
     end
  	if not isAreaClean(areaMin, areaMax) then
-		minetest.chat_send_player(name, orange("Please clean up your area before ploughing it."))
+		core.chat_send_player(name, orange("Please clean up your area before ploughing it."))
 		return
 	end
-	minetest.sound_play("farming_nextgen_seeder", {pos = pos, gain = farmingNG.gain,
+	core.sound_play("farming_nextgen_seeder", {pos = pos, gain = farmingNG.gain,
 			max_hear_distance = 10})
     charge = plough(areaMin, areaMax, charge)
-	--minetest.chat_send_all("Charge = "..dump(charge).."\nPer Node ="..perNode.."\n----------\n")
+	--core.chat_send_all("Charge = "..dump(charge).."\nPer Node ="..perNode.."\n----------\n")
 	if farmingNG.havetech then
 		if not technic.creative_mode then
 			meta:set_int("technic:charge", charge)
@@ -366,7 +366,7 @@ if farmingNG.havetech then
 	-- registration of technic tool and recipe
 	technic.register_power_tool("farming_nextgen:plough", farmingNG.plough_max_charge)
 
-	minetest.register_tool("farming_nextgen:plough", {
+	core.register_tool("farming_nextgen:plough", {
 		description = S("plough"),
 		inventory_image = "farming_nextgen_plough.png",
 		stack_max = 1,
@@ -376,10 +376,10 @@ if farmingNG.havetech then
 	    on_use = onUse,
 	  })
 
-	local mesecons_button = minetest.get_modpath("mesecons_button")
+	local mesecons_button = core.get_modpath("mesecons_button")
 	local trigger = mesecons_button and "mesecons_button:button_off" or "default:mese_crystal_fragment"
 
-	minetest.register_craft({
+	core.register_craft({
 		output = "farming_nextgen:plough",
 		recipe = {
 			{"technic:battery",            trigger,                  "technic:battery"},
@@ -390,7 +390,7 @@ if farmingNG.havetech then
 
 else -- registration of basic non technic tool and recipe
 		    
-    minetest.register_tool("farming_nextgen:plough", {
+    core.register_tool("farming_nextgen:plough", {
 	    description = S("plough"),
 	    groups = {soil = 2, crumbly = 1},
 	    inventory_image = "farming_nextgen_plough.png",
@@ -402,7 +402,7 @@ else -- registration of basic non technic tool and recipe
 	    on_use = onUse,
     })
 
-    minetest.register_craft({
+    core.register_craft({
 	    output = "farming_nextgen:plough",
 	    recipe = {
 		    {"default:diamondblock",			"default:mese_crystal_fragment",	"default:diamondblock"},
