aaf23c9f5b73e84566635ab0d7f8bcc1a264dba7
Gundul <sampah@mailbox.org>
2025-03-30 16:25:05 +0200

    unify indent, clean up code

--- init.lua
+++ init.lua
@@ -2,11 +2,10 @@
 
 farmingNG = {}
 local path = minetest.get_modpath(minetest.get_current_modname())
-
 if(minetest.get_translator ~= nil) then
-    farmingNG.S = minetest.get_translator(minetest.get_current_modname())
+	farmingNG.S = minetest.get_translator(minetest.get_current_modname())
 else
-    farmingNG.S = function ( s ) return s end
+	farmingNG.S = function ( s ) return s end
 end
 
 farmingNG.seeder_seed = {}
@@ -15,59 +14,53 @@ farmingNG.harvester_names = {}
 farmingNG.plough_pos = {pos1 = {}, pos2 = {}}
 farmingNG.version = "1.4.2"
 
-
 -- register_seed(Seed ,Plantname)
 -- Seed = fully name of the seed like "farming:seed_cotton"
 -- Plantname = fully Name of the plant like "farming:cotton_1"
-
 function farmingNG.register_seed(seed, plant)
-    if(seed ~= "" and plant ~= "") then
-       local new_seed = {}
-       new_seed = {seed, plant}
-       table.insert(farmingNG.seeder_seed, new_seed)
-       return true
-    end
-    return false
+	if(seed ~= "" and plant ~= "") then
+	   local new_seed = {}
+	   new_seed = {seed, plant}
+	   table.insert(farmingNG.seeder_seed, new_seed)
+	   return true
+	end
+	return false
 end
 
 -- register_util(Seedling, Util)
 -- Seedling = fully name of the seedling like "farming_nextgen:grape_seedling"
 -- Plantname = fully Name of the tool like "farming_plus:grapes_1"
-
 function farmingNG.register_util(seedling, util)
-    if(seedling ~= "" and util ~= "") then
-        local new_util = {}
-        new_util = {seedling, util}
-       table.insert(farmingNG.seeder_utils, new_util)
-       return true
-    end
-    
-    return false
-      
-end -- register_seed
-
+	if(seedling ~= "" and util ~= "") then
+		local new_util = {}
+		new_util = {seedling, util}
+	   table.insert(farmingNG.seeder_utils, new_util)
+	   return true
+	end
+	return false
+end
 
 dofile(path.."/settings.lua")
 dofile(path.."/machines/seeder.lua")
 
 if  farmingNG.harvester_machine then
-      dofile(path.."/machines/harvester.lua")
-      
-    function farmingNG.register_harvest(plantname)
-        if(plantname ~= "") then
-            farmingNG.harvester_names[plantname] = true
-            return true
-        end
-        return false
-    end
+	dofile(path.."/machines/harvester.lua")
+	  
+	function farmingNG.register_harvest(plantname)
+		if(plantname ~= "") then
+			farmingNG.harvester_names[plantname] = true
+			return true
+		end
+		return false
+	end
 else
-    function farmingNG.register_harvestername(plantname)
-        return false
-    end
+	function farmingNG.register_harvestername(plantname)
+		return false
+	end
 end
 
 if farmingNG.plough_machine then
-      dofile(path.."/machines/plough.lua")
+	dofile(path.."/machines/plough.lua")
 end
 
 -- The following is only on Jungle Server valid
@@ -76,6 +69,5 @@ if farmingNG.havetech then
 	minetest.register_alias("technic:seeder", "farming_nextgen:seeder")
 end
 
-print("[MOD] FarmingNG alias" .. minetest.get_current_modname() .. " loaded.")
-
-
+print("[MOD] FarmingNG alias ".. minetest.get_current_modname() .. 
+	" V".. farmingNG.version .." loaded.")
